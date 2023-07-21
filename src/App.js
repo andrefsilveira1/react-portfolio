f565c906531dda55eab3dd4443627b5813ab6733
André Freitas <freitasandre38@gmail.com>
2023-07-21 14:40:28 -0300

    fix: remove useless import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
 import LandPage from "./pages/home";
-import { HashLink as Link } from "react-router-hash-link";
 import Skills from "./components/skills";
 import Nav from "./components/nav"
 
