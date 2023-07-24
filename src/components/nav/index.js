7d8f683cf45736db22211116a5bdbef248ed123b
André Freitas <freitasandre38@gmail.com>
2023-07-24 19:20:26 -0300

    feat: create toggle dark/light mode

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HashLink as Link } from "react-router-hash-link";
 import "./index.css"
 
 export default function Nav() {
+  const [value, setValue] = useState(false);
+
+  function changeValue() {
+    setValue(!value);
+  }
   return (
     <nav className='top-nav'>
       <Link to="#home">
@@ -20,6 +25,10 @@ export default function Nav() {
       <Link to="#contact">
         Contact
       </Link>
+      <label class="relative inline-flex items-center cursor-pointer ml-5">
+        <input type="checkbox" value="" class="sr-only peer"  onChange={changeValue}/>
+        <div class="select-none w-10 h-3 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[-2px] after:left-[0px] after:bg-white after:rounded-full after:h-4 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+      </label>
     </nav>
   )
 }
