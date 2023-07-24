92c7a20e209ac9fe92e2644532e60616cf0ad58f
André Freitas <freitasandre38@gmail.com>
2023-07-24 19:06:59 -0300

    fix: change the last element of typewriter

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import HomeHeader from "../../components/header";
+import Typewriter from './typewriter';
 
 export default function LandPage() {
   return (
     <>
       <div id="home" className="text-white font-roboto grid justify-items-center">
-        <div className="min-h-screen text-white w-3/4">
+        <div className="min-h-screen text-white w-3/4 mb-5">
           <HomeHeader className="menu" />
         <div className='mt-5'>
           <h1>Hello there!</h1>
-          <p>Welcome to my portfolio, here I will show you something about projects developed by me</p>
+          <Typewriter delay={50} text="Welcome to my portfolio, here I will show you something about projects developed by Me!"/>
         </div>
         </div>
       </div>
