1ce760e9875981a07c1615bdc4fd85e98494a2e3
André Freitas <freitasandre38@gmail.com>
2023-07-24 19:42:21 -0300

    feat: add new theme color changing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter } from "react-router-dom";
 import LandPage from "./pages/home";
 import Skills from "./components/sections";
 import Nav from "./components/nav";
+import './App.css';
 
 
 function App() {
+  const [value, setValue] = useState(false);
+  const [transitionClass, setTransitionClass] = useState('');
+
+  function handleChangeValue(newValue) {
+    setValue(newValue);
+  }
+
+  useEffect(() => {
+    const transitionDelay = 300;
+    const timeout = setTimeout(() => {
+      setTransitionClass(value ? 'bg-white' : 'bg-black');
+    }, transitionDelay);
+
+    return () => clearTimeout(timeout);
+  }, [value]);
+
   return (
     <BrowserRouter>
-      <div className="flex bg-black justify-center">
-      <Nav/>
+      <div className={`flex justify-center transition-colors ${transitionClass}`}>
+      <Nav value={value} onChange={handleChangeValue}/>
       <div className="App bg-gray-900 text-white">
           <LandPage />
           <Skills/>
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import { HashLink as Link } from "react-router-hash-link";
 import "./index.css"
 
-export default function Nav() {
-  const [value, setValue] = useState(false);
+export default function Nav({propsValue, onChange}) {
+  const [value, setValue] = useState(propsValue);
 
   function changeValue() {
-    setValue(!value);
+    setValue(!value)
+    onChange(!value);
   }
   return (
     <nav className='top-nav'>
@@ -25,9 +26,9 @@ export default function Nav() {
       <Link to="#contact">
         Contact
       </Link>
-      <label class="relative inline-flex items-center cursor-pointer ml-5">
-        <input type="checkbox" value="" class="sr-only peer"  onChange={changeValue}/>
-        <div class="select-none w-10 h-3 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[-2px] after:left-[0px] after:bg-white after:rounded-full after:h-4 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+      <label className="relative inline-flex items-center cursor-pointer ml-5">
+        <input type="checkbox" value="" className="sr-only peer"  onChange={changeValue}/>
+        <div className="select-none w-10 h-3 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[-2px] after:left-[0px] after:bg-white after:rounded-full after:h-4 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
       </label>
     </nav>
   )
