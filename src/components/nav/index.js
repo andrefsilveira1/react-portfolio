105ff7a0b62a608ff775d1aed80101de2f1feb21
André Freitas <freitasandre38@gmail.com>
2023-07-24 19:51:46 -0300

    feat: create new icon of theme color

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { HashLink as Link } from "react-router-hash-link";
 import "./index.css"
+import { FaSun, FaMoon } from 'react-icons/fa';
+
 
 export default function Nav({propsValue, onChange}) {
   const [value, setValue] = useState(propsValue);
@@ -28,8 +30,13 @@ export default function Nav({propsValue, onChange}) {
       </Link>
       <label className="relative inline-flex items-center cursor-pointer ml-5">
         <input type="checkbox" value="" className="sr-only peer"  onChange={changeValue}/>
-        <div className="select-none w-10 h-3 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[-2px] after:left-[0px] after:bg-white after:rounded-full after:h-4 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+        <div className="select-none w-10 h-3 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[6px] after:left-[0px] after:bg-white after:rounded-full after:h-4 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+      { value ? <FaSun size={27} className='ml-4 text-blue-500'/> :
+        <FaMoon size={27} className='ml-4 text-blue-500'/>
+      }
       </label>
+      <Link>
+      </Link>
     </nav>
   )
 }
