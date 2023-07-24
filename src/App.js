d995b1cec86d95d42975c8a14978584918b75a6f
André Freitas <freitasandre38@gmail.com>
2023-07-24 18:51:48 -0300

    feat: change color theme

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Nav from "./components/nav";
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex justify-center">
+      <div className="flex bg-black justify-center">
       <Nav/>
       <div className="App bg-gray-900 text-white">
           <LandPage />
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import HomeHeader from "../../components/header";
-import "./index.css";
 
 export default function LandPage() {
   return (
@@ -8,6 +7,10 @@ export default function LandPage() {
       <div id="home" className="text-white font-roboto grid justify-items-center">
         <div className="min-h-screen text-white w-3/4">
           <HomeHeader className="menu" />
+        <div className='mt-5'>
+          <h1>Hello there!</h1>
+          <p>Welcome to my portfolio, here I will show you something about projects developed by me</p>
+        </div>
         </div>
       </div>
     </>
