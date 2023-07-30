933dcfdaefa3e69e1f3c1da193f3c18bd60e3485
André Freitas <freitasandre38@gmail.com>
2023-07-30 14:06:27 -0300

    refactor: changing top nav-bar

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,42 +1,55 @@
 import React, { useState } from 'react';
-import { HashLink as Link } from "react-router-hash-link";
-import "./index.css"
+import { HashLink as Link } from 'react-router-hash-link';
 import { FaSun, FaMoon } from 'react-icons/fa';
+import "./index.css"
 
-
-export default function Nav({propsValue, onChange}) {
+const Nav = ({ propsValue, onChange }) => {
   const [value, setValue] = useState(propsValue);
 
   function changeValue() {
-    setValue(!value)
+    setValue(!value);
     onChange(!value);
   }
+
   return (
-    <nav className='top-nav shadow'>
-      <Link to="#home">
-        Home
-      </Link>
-      <Link to="#skills">
-        Skills
-      </Link>
-      <Link to="#projects">
-        Projects
-      </Link>
-      <Link to="#about">
-        About me
-      </Link>
-      <Link to="#contact">
-        Contact
-      </Link>
-      <label className="relative inline-flex items-center cursor-pointer ml-5">
-        <input type="checkbox" value="" className="sr-only peer"  onChange={changeValue}/>
-        <div className="select-none w-10 h-3 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[6px] after:left-[0px] after:bg-white after:rounded-full after:h-4 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
-      { value ? <FaSun size={27} className='ml-5 text-blue-500'/> :
-        <FaMoon size={27} className='ml-5 text-blue-500'/>
-      }
-      </label>
-      <Link>
-      </Link>
+    <nav className="fixed top-0 left-0 w-full bg-white dark:bg-black shadow-md md:shadow-none">
+      <div className="container mx-auto px-4 py-2 md:py-4 md:flex md:items-center md:justify-between">
+        <div className="flex items-center">
+          <Link to="#home" className="mr-4 text-blue-500 md:mr-8 ">
+            Home
+          </Link>
+          <Link to="#skills" className="mr-4 text-blue-500 md:mr-8 ">
+            Skills
+          </Link>
+          <Link to="#projects" className="mr-4 text-blue-500 md:mr-8 ">
+            Projects
+          </Link>
+          <Link to="#about" className="mr-4 text-blue-500 md:mr-8 ">
+            About me
+          </Link>
+          <Link to="#contact" className="mr-4 text-blue-500 md:mr-8 ">
+            Contact
+          </Link>
+        </div>
+        <div className="flex items-center">
+          <label className="relative inline-flex items-center cursor-pointer ml-5">
+            <input
+              type="checkbox"
+              value=""
+              className="sr-only peer"
+              onChange={changeValue}
+            />
+            <div className="select-none w-10 h-3 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[6px] after:left-[0px] after:bg-white after:rounded-full after:h-4 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+            {value ? (
+              <FaSun size={27} className="ml-5 text-blue-500" />
+            ) : (
+              <FaMoon size={27} className="ml-5 text-blue-500" />
+            )}
+          </label>
+        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
+
+export default Nav;
